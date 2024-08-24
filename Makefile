7ce37a2ee31d42d01a388bf991c4759255c40448
zeindevs <zeindevs@gmail.com>
2024-08-24 10:01:31 +0700

    fix: update config and build command

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ run:
 build:
 	go build -ldflags="-s -w" -o bin/main.exe cmd/gospotify/main.go
 
-secret:
-	go run cmd/secret/main.go
+client:
+	go run cmd/client/main.go
 
 tailwind:
 	tailwindcss -i public/style.css -o public/static/css/style.css --config public/tailwind.config.js --minify --watch
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,8 +9,10 @@ import (
 )
 
 var (
-	port   = 5001
-	market = "ID"
+	port          = 5001
+	market        = "ID"
+	client_id     = ""
+	client_secret = ""
 )
 
 type Config struct {
@@ -22,16 +24,24 @@ type Config struct {
 
 func NewConfig() *Config {
 	var err error
-	portStr, ok := os.LookupEnv("PORT")
-	if ok {
-		port, err = strconv.Atoi(portStr)
+	if pot, ok := os.LookupEnv("PORT"); ok {
+		port, err = strconv.Atoi(pot)
 		util.ErrorPanic(err)
+	}
+	if mkt, ok := os.LookupEnv("MARKET"); ok {
+		market = mkt
+	}
+	if cid, ok := os.LookupEnv("CLIENT_ID"); ok {
+		client_id = cid
+	}
+	if csc, ok := os.LookupEnv("CLIENT_SECRET"); ok {
+		client_secret = csc
 	}
 
 	return &Config{
 		PORT:          port,
 		MARKET:        market,
-		CLIENT_ID:     os.Getenv("CLIENT_ID"),
-		CLIENT_SECRET: os.Getenv("CLIENT_SECRET"),
+		CLIENT_ID:     client_id,
+		CLIENT_SECRET: client_secret,
 	}
 }
